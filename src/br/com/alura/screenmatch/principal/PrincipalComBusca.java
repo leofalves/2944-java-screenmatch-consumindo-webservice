049511b9c02b0cdaf5b1c669bf65eaee46d74867
Leo Fabiano Alves <leo.fabiano@gmail.com>
2024-09-16 13:42:21 -0300

    Deserialization using GSON library

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,5 +1,12 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -24,7 +31,16 @@ public class PrincipalComBusca {
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
+        String json = response.body();
         System.out.println(response.toString());
-        System.out.println(response.body());
+        System.out.println(json);
+
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+        TituloOmdb tituloOmdb = gson.fromJson(json, TituloOmdb.class);
+        Titulo meuTitulo = new Titulo(tituloOmdb);
+
+        System.out.println(tituloOmdb);
+        System.out.println("-----------------------------");
+        System.out.println(meuTitulo);
     }
 }
