1fa1fc48a6ffca4e0db2278914153f844241aacf
Leo Fabiano Alves <leo.fabiano@gmail.com>
2024-09-16 12:36:16 -0300

    invoking APIs to retrieve a movie details and a recipe details

---
+++ src/br/com/alura/screenmatch/principal/PrincipalBuscaReceitas.java
@@ -0,0 +1,30 @@
+package br.com.alura.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalBuscaReceitas {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entre com o nome de uma receita para buscar:");
+        String nomeReceita = sc.nextLine();
+
+        String url = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + nomeReceita;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
+    }
+}
---
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -0,0 +1,30 @@
+package br.com.alura.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Entre com o nome do filme para pesquisa:");
+        var filme = leitura.nextLine();
+
+        String url = "https://www.omdbapi.com/?t=" + filme + "&apikey=c6c97426";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.toString());
+        System.out.println(response.body());
+    }
+}
