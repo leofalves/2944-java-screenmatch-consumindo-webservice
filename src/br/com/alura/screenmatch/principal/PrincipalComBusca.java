be01df246fa686285deec31d9d4596222ce2e59c
Leo Fabiano Alves <leo.fabiano@gmail.com>
2024-09-24 17:28:04 -0300

    Quick and dirty example of reading and writing files in file system

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -8,8 +8,7 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.net.URI;
 import java.net.URLEncoder;
 import java.net.http.HttpClient;
@@ -47,6 +46,33 @@ public class PrincipalComBusca {
             Titulo meuTitulo = new Titulo(tituloOmdb);
             System.out.println("-----------------------------");
             System.out.println(meuTitulo);
+
+            FileWriter escrita = new FileWriter( filme + ".txt");
+            escrita.write(meuTitulo.toString());
+            escrita.close();
+
+            File arquivo = new File(filme + ".txt");
+            System.out.println(arquivo.exists());
+
+            FileReader reader = new FileReader(arquivo);
+
+            System.out.println("------- LENDO O ARQUIVO -------");
+            int data = reader.read();
+            while (data != -1) {
+                System.out.print((char) data);
+                data = reader.read();
+            }
+            reader.close();
+
+
+/*
+*
+* O pacote java.io também fornece outras classes úteis, como:
+BufferedReader e BufferedWriter: são usadas para ler e gravar arquivos de texto de maneira eficiente, lendo e escrevendo uma linha por vez. Elas usam um buffer para armazenar os dados, o que torna a leitura e escrita mais rápida do que quando feita um caractere por vez;
+FileInputStream e FileOutputStream: são usadas para ler e gravar dados binários em um arquivo. Eles são usados para ler e gravar dados em arquivos que não são de texto, como imagens e arquivos de áudio;
+ObjectInputStream e ObjectOutputStream: são usadas para ler e gravar objetos em arquivos. Isso permite que você armazene objetos Java em arquivos para uso posterior ou para transferência entre diferentes aplicações.
+*
+* */
 
         } catch (NumberFormatException e) {
             System.out.println("Ocorreu um erro de conversão de números");
